ebbfb0bc3ee72537ee79a7104372ebe5778f7340
acer <diamadex@gmail.com>
2025-03-14 11:13:08 +0800

    - commented out some testing

--- Admin/barangay.php
+++ Admin/barangay.php
@@ -1,114 +1,114 @@
 <!-- <?php
 
-        require_once '../db/db.php';
+    //     require_once '../db/db.php';
 
 
-        $barangays = [
-            "Balintonga",
-            "Banisilon",
-            "Burgos",
-            "Calube",
-            "Caputol",
-            "Casusan",
-            "Conat",
-            "Culpan",
-            "Dalisay",
-            "Dullan",
-            "Ibabao",
-            "Labo",
-            "Lawaan",
-            "Lobogon",
-            "Lumbayao",
-            "Macubon",
-            "Makawa",
-            "Manamong",
-            "Matipaz",
-            "Maular",
-            "Mitazan",
-            "Mohon",
-            "Monterico",
-            "Nabuna",
-            "Ospital",
-            "Palayan",
-            "Pelong",
-            "Roxas",
-            "SanPedro",
-            "SantaAna",
-            "Sinampongan",
-            "Taguanao",
-            "Tawitawi",
-            "Toril",
-            "Tubod",
-            "Tuburan",
-            "Tugaya",
-            "Zamora"
-        ];
+    //     $barangays = [
+    //         "Balintonga",
+    //         "Banisilon",
+    //         "Burgos",
+    //         "Calube",
+    //         "Caputol",
+    //         "Casusan",
+    //         "Conat",
+    //         "Culpan",
+    //         "Dalisay",
+    //         "Dullan",
+    //         "Ibabao",
+    //         "Labo",
+    //         "Lawaan",
+    //         "Lobogon",
+    //         "Lumbayao",
+    //         "Macubon",
+    //         "Makawa",
+    //         "Manamong",
+    //         "Matipaz",
+    //         "Maular",
+    //         "Mitazan",
+    //         "Mohon",
+    //         "Monterico",
+    //         "Nabuna",
+    //         "Ospital",
+    //         "Palayan",
+    //         "Pelong",
+    //         "Roxas",
+    //         "SanPedro",
+    //         "SantaAna",
+    //         "Sinampongan",
+    //         "Taguanao",
+    //         "Tawitawi",
+    //         "Toril",
+    //         "Tubod",
+    //         "Tuburan",
+    //         "Tugaya",
+    //         "Zamora"
+    //     ];
 
-        try {
-            // $a = 104201001;
-            $startNum = 9000000000;
-            $userIDs = [];
-            $pdo->beginTransaction();
+    //     try {
+    //         // $a = 104201001;
+    //         $startNum = 9000000000;
+    //         $userIDs = [];
+    //         $pdo->beginTransaction();
 
-            foreach ($barangays as $bar) {
-                $pass = '@' . $bar;
-                $exec = [('Brgy. ' . $bar), (strtolower($bar) . '@gmail.com'), ($startNum++), strtolower($bar), password_hash($pass, PASSWORD_BCRYPT), 27];
-                // echo var_dump($exec);
+    //         foreach ($barangays as $bar) {
+    //             $pass = '@' . $bar;
+    //             $exec = [('Brgy. ' . $bar), (strtolower($bar) . '@gmail.com'), ($startNum++), strtolower($bar), password_hash($pass, PASSWORD_BCRYPT), 27];
+    //             // echo var_dump($exec);
 
-                // create user
-                $stmt = 'INSERT INTO users (full_name, email, mobile_num, username, password, role_id) VALUES (?,?,?,?,?,?)';
-                $stmt = $pdo->prepare($stmt);
-                $stmt->execute($exec);
-                $userIDs[] = $pdo->lastInsertId();
-                // echo $pass;
-                // echo '<br>';
-            }
+    //             // create user
+    //             $stmt = 'INSERT INTO users (full_name, email, mobile_num, username, password, role_id) VALUES (?,?,?,?,?,?)';
+    //             $stmt = $pdo->prepare($stmt);
+    //             $stmt->execute($exec);
+    //             $userIDs[] = $pdo->lastInsertId();
+    //             // echo $pass;
+    //             // echo '<br>';
+    //         }
 
-            // get all indicators of active
-            $stmt = 'SELECT i.keyctr as iid FROM maintenance_area_indicators i 
-    JOIN maintenance_criteria_setup mcs ON i.keyctr = mcs.indicator_keyctr 
-    JOIN maintenance_criteria_version mcv ON mcv.keyctr = mcs.version_keyctr
-    ';
-            $stmt = $pdo->query($stmt);
-            $inds = $stmt->fetchAll();
+    //         // get all indicators of active
+    //         $stmt = 'SELECT i.keyctr as iid FROM maintenance_area_indicators i 
+    // JOIN maintenance_criteria_setup mcs ON i.keyctr = mcs.indicator_keyctr 
+    // JOIN maintenance_criteria_version mcv ON mcv.keyctr = mcs.version_keyctr
+    // ';
+    //         $stmt = $pdo->query($stmt);
+    //         $inds = $stmt->fetchAll();
 
-            // get all barangay ids
-            $stmt = 'SELECT brgyid as bid FROM refbarangay';
-            $stmt = $pdo->query($stmt);
-            $bids = $stmt->fetchAll();
+    //         // get all barangay ids
+    //         $stmt = 'SELECT brgyid as bid FROM refbarangay';
+    //         $stmt = $pdo->query($stmt);
+    //         $bids = $stmt->fetchAll();
 
-            // permission insert statement
-            $permStmt = 'INSERT INTO permissions (assessment_submissions_create, assessment_submissions_read, assessment_submissions_update, assessment_submissions_delete, assessment_comments_read) VALUES (?, ?, ? , ? , ?)';
+    //         // permission insert statement
+    //         $permStmt = 'INSERT INTO permissions (assessment_submissions_create, assessment_submissions_read, assessment_submissions_update, assessment_submissions_delete, assessment_comments_read) VALUES (?, ?, ? , ? , ?)';
 
-            for ($i = 0; $i < count($userIDs); $i++) {
-                $uid = $userIDs[$i];
-                $bid = $bids[$i]['bid'];
-                // echo '<br>';
-                // echo $bids[0]['bid'];
-                // echo '<br>';
-                foreach ($inds as $iid) {
-                    echo $iid['iid'];
-                    echo '<br>';
-                    // create and get perm id
-                    $permExec = $pdo->prepare($permStmt);
-                    $permExec->execute(array_fill(0, 5, 1));
-                    $permId = $pdo->lastInsertId();
+    //         for ($i = 0; $i < count($userIDs); $i++) {
+    //             $uid = $userIDs[$i];
+    //             $bid = $bids[$i]['bid'];
+    //             // echo '<br>';
+    //             // echo $bids[0]['bid'];
+    //             // echo '<br>';
+    //             foreach ($inds as $iid) {
+    //                 echo $iid['iid'];
+    //                 echo '<br>';
+    //                 // create and get perm id
+    //                 $permExec = $pdo->prepare($permStmt);
+    //                 $permExec->execute(array_fill(0, 5, 1));
+    //                 $permId = $pdo->lastInsertId();
 
-                    // insert the user roles barangay perms
-                    $urbStmt = 'INSERT INTO user_roles_barangay (user_id, barangay_id, indicator_id, permission_id) VALUES (?, ?, ? , ? )';
-                    $urb = $pdo->prepare($urbStmt);
-                    $urb->execute([$uid, $bid, $iid['iid'], $permId]);
-                }
-            }
+    //                 // insert the user roles barangay perms
+    //                 $urbStmt = 'INSERT INTO user_roles_barangay (user_id, barangay_id, indicator_id, permission_id) VALUES (?, ?, ? , ? )';
+    //                 $urb = $pdo->prepare($urbStmt);
+    //                 $urb->execute([$uid, $bid, $iid['iid'], $permId]);
+    //             }
+    //         }
 
-            $pdo->commit();
-            echo 'yes';
-        } catch (\Throwable $th) {
-            if ($pdo->inTransaction()) {
-                $pdo->rollBack();
-            }
-            echo $th;
-        }
+    //         $pdo->commit();
+    //         echo 'yes';
+    //     } catch (\Throwable $th) {
+    //         if ($pdo->inTransaction()) {
+    //             $pdo->rollBack();
+    //         }
+    //         echo $th;
+    //     }
 
 // $stmt = 'SELECT * FROM refbarangay';
 // $stmt = $pdo->prepare($stmt);
--- Admin/create_auditor.php
+++ Admin/create_auditor.php
@@ -1,87 +1,87 @@
 <?php
-require_once '../db/db.php';
+// require_once '../db/db.php';
 
-try {
-  // if ($_SERVER['REQUEST_METHOD'] != 'POST') throw new Exception('Invalid request.');
-  global $pdo;
+// try {
+//   // if ($_SERVER['REQUEST_METHOD'] != 'POST') throw new Exception('Invalid request.');
+//   global $pdo;
 
-  $permissions = [
-    'assessment_submissions_read',
-    'assessment_comments_create',
-    'assessment_comments_read',
-    'assessment_comments_update',
-    'assessment_comments_delete',
-    'assessment_submissions_approve_disapprove',
-  ];
+//   $permissions = [
+//     'assessment_submissions_read',
+//     'assessment_comments_create',
+//     'assessment_comments_read',
+//     'assessment_comments_update',
+//     'assessment_comments_delete',
+//     'assessment_submissions_approve_disapprove',
+//   ];
 
-  // create permissions
-  $sql = 'INSERT INTO permissions (';
-  foreach ($permissions as $perm) {
-    $sql .= ' ' . $perm . ',';
-  }
-  // remove trailing comma
-  $sql = rtrim($sql, ',');
-  $sql .= ') VALUES (';
+//   // create permissions
+//   $sql = 'INSERT INTO permissions (';
+//   foreach ($permissions as $perm) {
+//     $sql .= ' ' . $perm . ',';
+//   }
+//   // remove trailing comma
+//   $sql = rtrim($sql, ',');
+//   $sql .= ') VALUES (';
 
-  // attach values
-  $true = array_fill(0, count($permissions), '?');
-  foreach ($true as $value) {
-    $sql .= ' ' . $value . ',';
-  }
+//   // attach values
+//   $true = array_fill(0, count($permissions), '?');
+//   foreach ($true as $value) {
+//     $sql .= ' ' . $value . ',';
+//   }
 
-  // remove trailing comma
-  $sql = rtrim($sql, ',');
-  $sql .= ')';
+//   // remove trailing comma
+//   $sql = rtrim($sql, ',');
+//   $sql .= ')';
 
 
-  $pdo->beginTransaction();
-  // insert permissions
-  $exec = array_fill(0, count($permissions), '1');
-  // echo var_dump($exec);
-  // echo '<br>';
-  // echo $sql;
-  // echo '<br>';
-  $stmt = $pdo->prepare($sql);
-  $stmt->execute($exec);
-  $perm_id = $pdo->lastInsertId();
+//   $pdo->beginTransaction();
+//   // insert permissions
+//   $exec = array_fill(0, count($permissions), '1');
+//   // echo var_dump($exec);
+//   // echo '<br>';
+//   // echo $sql;
+//   // echo '<br>';
+//   $stmt = $pdo->prepare($sql);
+//   $stmt->execute($exec);
+//   $perm_id = $pdo->lastInsertId();
 
-  // create user & get id
-  $stmt = $pdo->prepare('INSERT INTO users (full_name, username, email, password, role_id, mobile_num) VALUES (?, ?, ?, ?, ?, ?)');
-  $stmt->execute(['Master Auditor', 'aud', 'aud@gmail.com', password_hash('auditor', PASSWORD_BCRYPT), 28, '9123456780']);
-  $user_id = $pdo->lastInsertId();
+//   // create user & get id
+//   $stmt = $pdo->prepare('INSERT INTO users (full_name, username, email, password, role_id, mobile_num) VALUES (?, ?, ?, ?, ?, ?)');
+//   $stmt->execute(['Master Auditor', 'aud', 'aud@gmail.com', password_hash('auditor', PASSWORD_BCRYPT), 28, '9123456780']);
+//   $user_id = $pdo->lastInsertId();
 
-  // get all barangays
-  $stmt = $pdo->query('SELECT brgyid AS bid FROM refbarangay');
-  $bars = $stmt->fetchAll();
-  echo 'Bars is: ';
-  foreach ($bars as $bar) {
-    echo var_dump($bar);
-    echo '<br>';
-  }
+//   // get all barangays
+//   $stmt = $pdo->query('SELECT brgyid AS bid FROM refbarangay');
+//   $bars = $stmt->fetchAll();
+//   echo 'Bars is: ';
+//   foreach ($bars as $bar) {
+//     echo var_dump($bar);
+//     echo '<br>';
+//   }
 
-  // get all indicators (doesn't consider active version cuz i'm lazy rn)
-  $stmt = $pdo->query('SELECT mai.keyctr AS iid FROM maintenance_area_indicators mai JOIN maintenance_criteria_setup mcs ON mcs.indicator_keyctr = mai.keyctr');
-  $inds = $stmt->fetchAll();
-  // echo 'Inds is: ';
-  // echo $inds;
-  $inds = array_unique($inds, SORT_REGULAR);
-  foreach ($inds as $ind) {
-    // echo 'per item';
-    // echo var_dump($ind);
-    // echo '<br>';
-  }
+//   // get all indicators (doesn't consider active version cuz i'm lazy rn)
+//   $stmt = $pdo->query('SELECT mai.keyctr AS iid FROM maintenance_area_indicators mai JOIN maintenance_criteria_setup mcs ON mcs.indicator_keyctr = mai.keyctr');
+//   $inds = $stmt->fetchAll();
+//   // echo 'Inds is: ';
+//   // echo $inds;
+//   $inds = array_unique($inds, SORT_REGULAR);
+//   foreach ($inds as $ind) {
+//     // echo 'per item';
+//     // echo var_dump($ind);
+//     // echo '<br>';
+//   }
 
-  // create new user_roles_barangay
-  foreach ($bars as $bar) {
-    foreach ($inds as $ind) {
-      // insert
-      $stmt = $pdo->prepare('INSERT INTO user_roles_barangay (user_id, barangay_id, indicator_id, permission_id) VALUES (?, ?, ?, ?)');
-      $stmt->execute([$user_id, $bar['bid'], $ind['iid'], $perm_id]);
-    }
-  }
-  $pdo->commit();
-  echo 'aaaaaaaaaaaaaaaaaaaaaaaaaaaa';
-} catch (\Throwable $th) {
-  if ($pdo->inTransaction()) $pdo->rollBack();
-  echo $th;
-}
+//   // create new user_roles_barangay
+//   foreach ($bars as $bar) {
+//     foreach ($inds as $ind) {
+//       // insert
+//       $stmt = $pdo->prepare('INSERT INTO user_roles_barangay (user_id, barangay_id, indicator_id, permission_id) VALUES (?, ?, ?, ?)');
+//       $stmt->execute([$user_id, $bar['bid'], $ind['iid'], $perm_id]);
+//     }
+//   }
+//   $pdo->commit();
+//   echo 'aaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+// } catch (\Throwable $th) {
+//   if ($pdo->inTransaction()) $pdo->rollBack();
+//   echo $th;
+// }
